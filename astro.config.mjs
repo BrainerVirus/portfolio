f8cf45a41528ce181a140afe4310ed002661de6e
Cristhofer Pincetti <github.annotate387@passmail.net>
2023-11-14 18:29:55 -0300

    feat(ssr): add vercel ssr integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-
 import prefetch from "@astrojs/prefetch";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx(), prefetch()],
   server: {
-    port: 3000,
+    port: 3000
   },
+  output: "server",
+  adapter: vercel()
 });
