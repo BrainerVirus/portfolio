8dd612efb5eca7563ff9c1ba7b61e8c58d311efc
Cristhofer Pincetti <github.annotate387@passmail.net>
2023-11-14 18:42:22 -0300

    build(astro config): remove node ssr adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,10 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import prefetch from "@astrojs/prefetch";
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx(), prefetch()],
   server: {
-    port: 3000
+    port: 3000,
   },
-  output: "server",
-  adapter: vercel()
 });
