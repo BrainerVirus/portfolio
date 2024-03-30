24afc14f5492cb9e805c2f736070646930d60d58
Cristhofer Pincetti <github.annotate387@passmail.net>
2024-03-30 12:53:18 -0300

    chore(package.json): add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,15 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import icon from "astro-icon";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx(), icon()],
   prefetch: true,
   server: {
-    port: 3000,
+    port: 3000
   },
+  output: "server",
+  adapter: vercel()
 });
