fd09936d1f4ebe058ae8150b1a0476ed07e3ac25
Cristhofer Pincetti <86972354+BrainerVirus@users.noreply.github.com>
2024-07-14 18:56:38 -0400

    Feature/refactor home page (#16)
    * chore: add compilerOptions for path completition
    
    * build(package.json): update packages and set specific version
    
    * chore: update gitignore
    
    * build: remove pnpm-lock.yaml from tracking
    
    * chore: add linter and formmating rules
    
    * chore: add more linter, and formatter settings
    
    * style: refactor global styles into one css file and import it in the layout
    
    * style(tailwind.config.js): add font family source sans pro
    
    * fix: remove typo from settings.json and add precommit for husky
    
    * refactor(tsconfig.json): remove public dir exclusion
    
    * build(astro.config.mjs): add tailwind settings to use custom css file
    
    * refactor(index.astro): update styles, layout, and text content
    
    * test(home-page.spect.ts): fix tests

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,33 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import mdx from "@astrojs/mdx";
-import icon from "astro-icon";
+import mdx from "@astrojs/mdx"
+import tailwind from "@astrojs/tailwind"
+import icon from "astro-icon"
+import { defineConfig } from "astro/config"
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/serverless"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), icon()],
-  prefetch: true,
-  server: {
-    port: 3000,
-  },
-  output: "hybrid",
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-    maxDuration: 8,
-    imageService: true,
-    devImageService: "squoosh",
-    isr: {
-      expiration: 60 * 60 * 24,
-    },
-  }),
-});
+	integrations: [
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		mdx(),
+		icon(),
+	],
+	prefetch: true,
+	server: {
+		port: 3000,
+	},
+	output: "hybrid",
+	adapter: vercel({
+		webAnalytics: {
+			enabled: true,
+		},
+		maxDuration: 8,
+		imageService: true,
+		devImageService: "squoosh",
+		isr: {
+			expiration: 60 * 60 * 24,
+		},
+	}),
+})
