08225495b76448292b5b8ddc0a393f98e18db23c
Cristhofer Pincetti <86972354+BrainerVirus@users.noreply.github.com>
2024-07-14 18:56:38 -0400

    Feature/refactor home page (#16)
    * chore: add compilerOptions for path completition
    
    * build(package.json): update packages and set specific version
    
    * chore: update gitignore
    
    * build: remove pnpm-lock.yaml from tracking
    
    * chore: add linter and formmating rules
    
    * chore: add more linter, and formatter settings
    
    * style: refactor global styles into one css file and import it in the layout
    
    * style(tailwind.config.js): add font family source sans pro
    
    * fix: remove typo from settings.json and add precommit for husky
    
    * refactor(tsconfig.json): remove public dir exclusion
    
    * build(astro.config.mjs): add tailwind settings to use custom css file
    
    * refactor(index.astro): update styles, layout, and text content
    
    * test(home-page.spect.ts): fix tests

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,32 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {},
-    colors: {
-      text: {
-        primary: "#f0f8ff",
-        foreground: "",
-      },
-      background: "#141416",
-      foreground: "",
-      primary: {
-        DEFAULT: "#f0f8ff",
-        foreground: "#141416",
-      },
-      secondary: {
-        DEFAULT: "hsl(57.2, 95.1%, 48.2%)",
-        foreground: "",
-      },
-      muted: {
-        DEFAULT: "",
-        foreground: "",
-      },
-      accent: {
-        DEFAULT: "hsl(279, 71%, 45%)",
-        foreground: "",
-      },
-    },
-  },
-  plugins: [],
-};
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		extend: {
+			fontFamily: {
+				"source-sans-pro": ["Source Sans Pro", "Arial", "Helvetica", "sans-serif"],
+			},
+		},
+		colors: {
+			text: {
+				primary: "#f0f8ff",
+				foreground: "",
+			},
+			background: "#141416",
+			foreground: "",
+			primary: {
+				DEFAULT: "#f0f8ff",
+				foreground: "#141416",
+			},
+			secondary: {
+				DEFAULT: "hsl(57.2, 95.1%, 48.2%)",
+				foreground: "",
+			},
+			muted: {
+				DEFAULT: "",
+				foreground: "",
+			},
+			accent: {
+				DEFAULT: "hsl(279, 71%, 45%)",
+				foreground: "",
+			},
+		},
+	},
+	plugins: [],
+}
