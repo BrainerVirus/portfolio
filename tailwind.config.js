98f37a62dd5bcb8c125dad5f8f8aeaceb0acada2
Cristhofer Pincetti <github.annotate387@passmail.net>
2024-08-29 22:46:26 -0400

    style: add tailwind animations plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
@@ -95,7 +96,8 @@ export default {
 		],
 	},
 	plugins: [
-		require("tailwindcss-animated"),
+		// require("tailwindcss-animated"),
+		require("@midudev/tailwind-animations"),
 		require("@tailwindcss/typography"),
 		require("daisyui"),
 	],
