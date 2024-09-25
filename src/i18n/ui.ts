f48d8f068b633f41baf5fc9349405b99453d6c12
Cristhofer Pincetti <github.annotate387@passmail.net>
2024-09-24 21:20:31 -0300

    feat(i18n): add es/en texts and logic to translate ui text

---
+++ src/i18n/ui.ts
@@ -0,0 +1,15 @@
+export const languages = {
+	en: "English",
+	es: "Spanish",
+}
+
+export const defaultLang = "en"
+
+export const ui = {
+	en: {
+		"nav.contact": "Contact",
+	},
+	es: {
+		"nav.contact": "Contacto",
+	},
+} as const
