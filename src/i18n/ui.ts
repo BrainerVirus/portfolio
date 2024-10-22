b78526aeefdbf781d3e5208ce1c549b4249b9faf
Cristhofer Pincetti <github.annotate387@passmail.net>
2024-10-21 21:38:04 -0300

    feat(alert.astro): add translation for ui alert work in progress

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -8,8 +8,10 @@ export const defaultLang = "en"
 export const ui = {
 	en: {
 		"nav.contact": "Contact",
+		"altert.wip": "WIP stay tuned for the exciting final result!.",
 	},
 	es: {
 		"nav.contact": "Contacto",
+		"altert.wip": "¡En construcción! ¡Estén atentos para el emocionante resultado final!.",
 	},
 } as const
