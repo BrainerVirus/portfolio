9873f3fb9a172a37538f36bbc3b61613b15a626d
Cristhofer Pincetti <github.annotate387@passmail.net>
2025-04-19 15:45:42 -0400

    refactor(styles): reorganize Tailwind CSS integration and remove unused files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 import mdx from "@astrojs/mdx"
-import tailwind from "@astrojs/tailwind"
 import icon from "astro-icon"
 import { defineConfig, passthroughImageService } from "astro/config"
 
 import vercel from "@astrojs/vercel"
+
+import tailwindcss from "@tailwindcss/vite"
 
 // https://astro.build/config
 export default defineConfig({
 	image: {
 		service: passthroughImageService(),
 	},
+
 	i18n: {
 		defaultLocale: "en",
 		locales: ["es", "en"],
@@ -18,17 +20,19 @@ export default defineConfig({
 			redirectToDefaultLocale: false,
 		},
 	},
-	integrations: [
-		tailwind({
-			applyBaseStyles: false,
-		}),
-		mdx(),
-		icon(),
-	],
+
+	integrations: [mdx(), icon()],
+
 	prefetch: true,
+
 	server: {
 		port: 3000,
 	},
+
 	output: "server",
 	adapter: vercel(),
+
+	vite: {
+		plugins: [tailwindcss()],
+	},
 })
